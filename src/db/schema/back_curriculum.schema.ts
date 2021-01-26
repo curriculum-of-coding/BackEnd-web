48bc8ab058bdb89ed32e2718101a360abfef48bf
SYChoi <sychoi1996@naver.com>
2021-01-26 23:20:35 +0900

    back_curriculum schema created

--- src/db/schema/back_curriculum.schema.ts
+++ src/db/schema/back_curriculum.schema.ts
@@ -3,31 +3,29 @@ import { model, Schema, connection } from 'mongoose';
 let autoIncrement = require('mongoose-auto-increment');
 autoIncrement.initialize(connection);
 
-interface backQA {
-	FA_ID: number;
-	PARENT_ID: string;
+interface backCurriculum {
+	FC_ID: number;
 	TITLE: string;
 	CONTENT: string;
 	REG_USER_ID: string;
 	REG_DATE: Date;
 };
 
-const backQASchema: Schema = new Schema({
-	FA_ID: { type: Number, unique: true },
-	PARENT_ID: Schema.Types.ObjectId,
+const backCurriculumSchema: Schema = new Schema ({
+	FC_ID: { type: Number, unique: true },
 	TITLE: { type: String, default: NULL, maxLength: 150 },
 	CONTENT: { type: String, default: NULL, maxLength: 4000 },
 	REG_USER_ID: Schema.Types.ObjectId,
 	REG_DATE: { type: Date, default: Date.now }
 });
-backQASchema.index({ FA_ID: 1, TITLE: 1 });
-backQASchema.plugin( autoIncrement.plugin, {
-	model: 'back_QA',
-	field: 'FA_ID',
+backCurriculumSchema.index({ FC_ID: 1, TITLE: 1 });
+backCurriculumSchema.plugin( autoIncrement.plugin, {
+	model: 'back_curriculum',
+	field: 'FC_ID',
 	startAt: 1,
 	increment: 1
 });
 
-const BackQASchema = model('back_QA', backQASchema);
+const BackCurriculumSchema = model('back_curriculum', backCurriculumSchema);
 
-export { BackQASchema, backQA };
+export { BackCurriculumSchema, backCurriculum };
