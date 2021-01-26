84de33301f4aa8890347885027513f45670eea89
SYChoi <sychoi1996@naver.com>
2021-01-26 14:29:43 +0900

    mongoose-auto-increment added, User_INFO & back_freeboard schema added

---
+++ src/db/schema/User_INFO.schema.ts
@@ -0,0 +1,56 @@
+import { model, Schema } from 'mongoose';
+
+interface User_INFO {
+	USER_ID: string;
+	USER_PWD: string;
+	EMAIL: string;
+	GITHUB_MAIL: string;
+	GITHUB_AUTH_KEY: string;
+	KAKAO_MAIL: string;
+	KAKAO_AUTH_KEY: string;
+	GOOGLE_MAIL: string;
+	GOOGLE_AUTH_KEY: string;
+	PWD_QUEST_TYPE: number;
+	PWD_ANSWER: string;
+	INTEREST_FRONT: boolean;
+	INTEREST_BACK: boolean;
+	INTEREST_DEVOPS: boolean;
+	TOS_YN: boolean;
+	PP_YN: boolean;
+	REG_DATE: Date;
+	UPD_DATE: Date;
+	SUPER_USER: boolean;
+}
+
+let pwdQuestion: Array<string> = [
+	"Where did you born?",
+	"When is your birth?"
+];
+
+const userInfoSchema: Schema = new Schema({
+	USER_ID: Schema.Types.ObjectId,
+	NICKNAME: { type: String, required: true, trim: true, default: },
+	USER_PWD: { type: String, required: true, trim: true, maxLength: 100 },
+	EMAIL: { type: String, required: true, unique: true },
+	GITHUB_MAIL: { type: String, unique: true },
+	GITHUB_AUTH_KEY: { type: String },
+	KAKAO_MAIL: { type: String, unique: true},
+	KAKAO_AUTH_KEY: { type: String },
+	GOOGLE_MAIL: { type: String, unique: true },
+	GOOGLE_AUTH_KEY: { type: String },
+	PWD_QUEST_TYPE: { type: Number, required: true, enum: pwdQuestion },
+	PWD_ANSWER: { type: String, required: true, default: NULL },
+	INTEREST_FRONT: { type: Boolean, default: NULL },
+	INTEREST_BACK: { type: Boolean, default: NULL },
+	INTEREST_DEVOPS: { type: Boolean, default: NULL },
+	TOS_YN: { type: Boolean },
+	PP_YN: { type: Boolean },
+	REG_DATE: { type: Date, default: Date.now },
+	UPD_DATE: { type: Date, default: NULL },
+	SUPER_USER: { type: Boolean, default: false },
+});
+userInfoSchema.index({ EMAIL: 1, GITHUB_MAIL: 1, KAKAO_MAIL: 1, GOOGLE_MAIL: 1 });
+
+const UserInfoSchema = model('User_INFO', userInfoSchema);
+
+export { UserInfoSchema, UserINFO };
---
+++ src/db/schema/back_freeboard.schema.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+let autoIncrement = require('mongoose-auto-increment');
+
+interface back_freeboard {
+	FFB_ID: string;
+	TITLE: string;
+	CONTENT: string;
+	REG_USER_ID: string;
+	REG_DATE: Date;
+}
+
+const backFreeBoardSchema: Schema = new Schema({
+	FFB_ID: { type: Number, required: true, unique: true },
+	TITLE: { type: String, required: true, default: NULL, maxLength: 150 },
+	CONTENT: { type: String, default: NULL, maxLength: 4000 },
+	REG_USER_ID: Schema.Types.ObjectId,
+	REG_DATE: { type: Date, default: Date.now }
+});
+backFreeBoardSchema.index({ FFB_ID: 1, TITLE: 1 });
+backFreeBoardSchema.plugin( autoIncrement.plugin, {
+	model: 'backFreeBoardSchema',
+	field: 'FFB_ID',
+	startAt: 1,
+	increment: 1
+});
+
+const BackFreeBoardSchema = model('back_freeboard', backFreeBoardSchema);
+
+export { BackFreeBoardSchema, back_freeboard };
