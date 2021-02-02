08a4b31cfa2565baaa1b0ce8f44dcc5fffdd0388
lethekawa <lethekawa@gmail.com>
2021-02-02 18:57:34 +0900

    typo : eslint, FileUploadSchema

--- src/db/schema/file.upload.ts
+++ src/db/schema/file.upload.ts
@@ -14,12 +14,12 @@ const fileUploadSchema: Schema = new Schema({
     CUS_ID: { type: String, required: true, unique: true },
     CUS_SEQ: { type: Number, required: true, unique: true },
     FILE_NAME: { type: String, required: true, trim: true },
-    FILE_PATH: { type: String, required: true, trim: true},
+    FILE_PATH: { type: String, required: true, trim: true },
     FILE_NAME_UUID: { type: Number, required: true, default: 1, min: 0, max: 250 },
     REG_USER_ID: { type: String, required: true, unique: true },
-    REG_DATE: { type: Date, default: Date.now, required: true, },
+    REG_DATE: { type: Date, default: Date.now, required: true },
 });
 
 const FileUploadSchema = model('fileUpload', fileUploadSchema);
 
-export { fileUploadSchema, fileUpload };
+export { FileUploadSchema, fileUpload };
