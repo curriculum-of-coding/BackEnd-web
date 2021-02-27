d5d476f03782fee4fe717f04e8b4794acee1bcc2
SYChoi <sychoi1996@naver.com>
2021-02-27 14:43:03 +0900

    fix: qa schema renewed

--- src/db/schema/qa.schema.ts
+++ src/db/schema/qa.schema.ts
@@ -1,19 +1,17 @@
 import { model, Schema } from 'mongoose';
 
 interface QA {
-    PARENT_ID: number;
-    TITLE: string;
-    CONTENT: string;
-    REG_USER_ID: number;
-    REG_DATE: Date;
+    title: string;
+    content: string;
+    regUser: number;
+    regDate: Date;
 }
 
 const _QASchema: Schema = new Schema({
-    PARENT_ID: { type: Number, default: null },
-    TITLE: { type: String, default: null, maxLength: 150 },
-    CONTENT: { type: String, default: null, maxLength: 4000 },
-    REG_USER_ID: { type: Number, required: true },
-    REG_DATE: { type: Date, default: Date.now },
+    title: { type: String, default: null, maxLength: 150 },
+    content: { type: String, default: null, maxLength: 4000 },
+    regUser: { type: Schema.Types.ObjectId, required: true },
+    regDate: { type: Date, default: Date.now },
 });
 _QASchema.index({ TITLE: 1 });
 
