57fb2c1136c85f555f8407bfde4b1c67c2e160c6
SYChoi <sychoi1996@naver.com>
2021-02-28 15:07:06 +0900

    fix: boardWrap schema object -> Array<object>

--- src/db/schema/boardWrap.schema.ts
+++ src/db/schema/boardWrap.schema.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from 'mongoose';
 
 interface boardWrap {
-    curriculum: string;
-    freeboard: string;
-    qa: string;
+    curriculum: Array<string>;
+    freeboard: Array<string>;
+    qa: Array<string>;
     type: string;
 }
 
@@ -11,9 +11,9 @@ const domaintypes: Array<string> = ['backend', 'frontend', 'devOps'];
 export const countDomainTypes = domaintypes.length;
 
 const boardWrapSchema: Schema = new Schema({
-    curriculum: { type: Schema.Types.ObjectId, ref: 'curriculum', required: true },
-    freeboard: { type: Schema.Types.ObjectId, ref: 'freeboard', required: true },
-    qa: { type: Schema.Types.ObjectId, ref: 'qa', required: true },
+    curriculum: { type: [Schema.Types.ObjectId], ref: 'curriculum', required: true },
+    freeboard: { type: [Schema.Types.ObjectId], ref: 'freeboard', required: true },
+    qa: { type: [Schema.Types.ObjectId], ref: 'qa', required: true },
     type: { type: String, required: true, enum: domaintypes },
 });
 
