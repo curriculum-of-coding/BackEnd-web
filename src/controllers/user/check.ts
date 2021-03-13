39ebe9f7e2657c11d642224999f62807048f927c
lethekawa <lethekawa@gmail.com>
2021-03-13 15:03:58 +0900

    Fix : check API type check in array

--- src/controllers/user/check.ts
+++ src/controllers/user/check.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { UserInfoSchema } from '../../db/schema/userInfo.schema';
 import { HTTPError } from '../../types/error';
 import { HTTPResult } from '../../types/result';
+
+const queryType: string[] = ['email', 'nickname'];
+
 /**
  *
  * @param {Request} req
@@ -11,16 +14,18 @@ import { HTTPResult } from '../../types/result';
 export async function CheckUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     const type: string = req.query['type']?.toString();
 
-    if (type === 'email') {
-        const email: string = req.query['email']?.toString();
-        const docs = await UserInfoSchema.find({
-            email: email,
-        }).exec();
+    if (queryType.includes(type)) {
+        const key: string = req.query[type]?.toString();
+        if (key) {
+            const docs = await UserInfoSchema.find({ [type]: key }).exec();
 
-        if (docs && docs.length) {
-            next(new HTTPError(400, 'id is overlap'));
+            if (docs && docs.length) {
+                next(new HTTPError(400, `${type} is overlap`));
+            } else {
+                next(new HTTPResult(200, `${type} is not overlap`));
+            }
         } else {
-            next(new HTTPResult(200, 'id is not overlap'));
+            next(new HTTPError(400, 'find value is undefined'));
         }
     } else {
         next(new HTTPError(400, 'find type is undefined'));
